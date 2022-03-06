90d90304d8479fe7eb6aecf6f50aae6e20917249
micael batista <micaelsbv8@gmail.com>
2022-03-06 13:35:50 -0300

    first commit

---
+++ CadastroLogin/Models/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace CadastroLogin.Models.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
